fd09c05599d2d2fd4155ac4f1b52d3f838097617
Georg Freitag <georg.freitag@gmail.com>
2019-09-02 14:32:17 +0200

    create talents

--- src/character/talents.js
+++ src/character/talents.js
@@ -1,8 +1,86 @@
 export const talents = [
   {
-    name: "Korpertalente",
+    name: "Körpertalente",
     subTalents: [
-      { name: "fliegen", value: 0, handicap: false, attributes: [0, 1, 2] }
+      { name: "Fliegen", handicap: 1, attributes: [0, 2, 5] },
+      { name: "Gaukeleien", handicap: 1, attributes: [0, 3, 4] },
+      { name: "Klettern", handicap: 1, attributes: [0, 5, 7] },
+      { name: "Körperbeherrschung", handicap: 1, attributes: [5, 5, 6] },
+      { name: "Kraftakt", handicap: 1, attributes: [6, 7, 7] },
+      { name: "Reiten", handicap: 1, attributes: [3, 5, 7] },
+      { name: "Schwimmen", handicap: 1, attributes: [5, 6, 7] },
+      { name: "Selbstbeherrschung", handicap: 0, attributes: [0, 0, 6] },
+      { name: "Singen", handicap: 2, attributes: [1, 3, 6] },
+      { name: "Sinnesschärfe", handicap: 2, attributes: [1, 2, 2] },
+      { name: "Tanzen", handicap: 1, attributes: [1, 3, 5] },
+      { name: "Taschendiebstahl", handicap: 1, attributes: [0, 4, 5] },
+      { name: "Verbergen", handicap: 1, attributes: [0, 2, 5] },
+      { name: "Zechen", handicap: 0, attributes: [1, 6, 7] }
+    ]
+  },
+  {
+    name: "Gesellschaftstalente",
+    subTalents: [
+      { name: "Bekehren & Überzeugen", handicap: 0, attributes: [0, 1, 3] },
+      { name: "Betören", handicap: 2, attributes: [0, 3, 3] },
+      { name: "Einschüchtern", handicap: 0, attributes: [0, 2, 3] },
+      { name: "Etikette", handicap: 2, attributes: [1, 2, 3] },
+      { name: "Gassenwissen", handicap: 2, attributes: [1, 2, 3] },
+      { name: "Menschenkenntnis", handicap: 0, attributes: [1, 2, 3] },
+      { name: "Überreden", handicap: 0, attributes: [0, 2, 3] },
+      { name: "Verkleiden", handicap: 2, attributes: [2, 3, 5] },
+      { name: "Willenskraft", handicap: 0, attributes: [0, 2, 3] }
+    ]
+  },
+  {
+    name: "Naturtalente",
+    subTalents: [
+      { name: "Fährtensuchen", handicap: 1, attributes: [0, 2, 5] },
+      { name: "Fesseln", handicap: 2, attributes: [1, 4, 7] },
+      { name: "Fischen & Angeln", handicap: 2, attributes: [4, 5, 6] },
+      { name: "Orientierung", handicap: 0, attributes: [1, 2, 2] },
+      { name: "Pflanzenkunde", handicap: 2, attributes: [1, 4, 6] },
+      { name: "Tierkunde", handicap: 1, attributes: [0, 0, 3] },
+      { name: "Wildnisleben", handicap: 1, attributes: [0, 5, 6] }
+    ]
+  },
+  {
+    name: "Wissenstalente",
+    subTalents: [
+      { name: "Brett- & Glücksspiel", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Geographie", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Geschichtswissen", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Götter & Kulte", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Kriegskunst", handicap: 0, attributes: [0, 1, 2] },
+      { name: "Magiekunde", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Mechanik", handicap: 0, attributes: [1, 1, 4] },
+      { name: "Rechnen", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Rechtskunde", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Sagen & Legenden", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Sphärenkunde", handicap: 0, attributes: [1, 1, 2] },
+      { name: "Sternkunde", handicap: 0, attributes: [1, 1, 2] }
+    ]
+  },
+  {
+    name: "Handwerkstalente",
+    subTalents: [
+      { name: "Alchimie", handicap: 1, attributes: [0, 1, 4] },
+      { name: "Boote & Schiffe", handicap: 1, attributes: [4, 5, 7] },
+      { name: "Fahrzeuge", handicap: 1, attributes: [3, 4, 6] },
+      { name: "Handel", handicap: 0, attributes: [1, 2, 3] },
+      { name: "Heilkunde Gift", handicap: 1, attributes: [0, 1, 2] },
+      { name: "Heilkunde Krankheiten", handicap: 1, attributes: [0, 2, 6] },
+      { name: "Heilkunde Seele", handicap: 0, attributes: [2, 3, 6] },
+      { name: "Heilkunde Wunden", handicap: 1, attributes: [2, 4, 4] },
+      { name: "Holzbearbeitung", handicap: 1, attributes: [4, 5, 7] },
+      { name: "Lebensmittelbearbeitung", handicap: 1, attributes: [2, 4, 4] },
+      { name: "Lederbearbeitung", handicap: 1, attributes: [4, 5, 6] },
+      { name: "Malen & Zeichnen", handicap: 1, attributes: [2, 4, 4] },
+      { name: "Metallbearbeitung", handicap: 1, attributes: [4, 6, 7] },
+      { name: "Musizieren", handicap: 1, attributes: [3, 4, 6] },
+      { name: "Schlösser knacken", handicap: 1, attributes: [2, 4, 4] },
+      { name: "Steinbearbeitung", handicap: 1, attributes: [4, 4, 7] },
+      { name: "Stoffbearbeitung", handicap: 1, attributes: [1, 4, 4] }
     ]
   }
 ];
