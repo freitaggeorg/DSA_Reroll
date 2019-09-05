065f0616a35ff1c48a537e6dfa08fef649486cbc
Unknown <schnitzel128@gmail.com>
2019-09-05 12:23:33 +0200

    Deployment

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import CharacterVerwaltung from "./views/CharacterVerwaltung";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes: [
     {
